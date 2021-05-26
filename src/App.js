5cb7c4f1c955c8090dbefd2c16ee00680f439eef
Rebecca Choo <rnwldms@gmail.com>
2021-05-26 11:44:04 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+	return (
+		<div className="m-6">
+			<div className="grid grid-cols-3 gap-6">
+				<div className="h-16 rounded-lg bg-pink-500"></div>
+				<div className="h-16 rounded-lg bg-pink-500"></div>
+				<div className="h-16 rounded-lg bg-pink-500"></div>
+			</div>
+		</div>
+	);
+}
+
+export default App;
